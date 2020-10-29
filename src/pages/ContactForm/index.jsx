1cecb071e81883265bbc09bc3c551ff9d01fab51
Lucas Cavalcante <lucascavalcante@gmail.com>
2020-10-29 01:46:24 -0300

    Fix the return of the API to show only success message

--- src/pages/ContactForm/index.jsx
+++ src/pages/ContactForm/index.jsx
@@ -9,6 +9,7 @@ import "./index.css";
 function ContactForm() {
 	const [values, setValues] = useState(null);
 	const [message, setMessage] = useState("");
+	const [success, setSuccess] = useState(false);
 	const { cities, setState, isLoading, setIsLoading } = useCity();
 	const states = [
 		"AB",
@@ -41,6 +42,7 @@ function ContactForm() {
 			.post("contact-form", values)
 			.then((res) => {
 				setMessage(res.data.message);
+				setSuccess(true);
 			})
 			.catch((error) => {
 				const arrErrors = error.response.data.errors;
@@ -58,85 +60,90 @@ function ContactForm() {
 					<div>Wait...</div>
 				</div>
 			)}
-			<div className="container">
-				<div>{message}</div>
-				<div className="formContact">
-					<form onSubmit={handleSubmit}>
-						<div>
-							First Name <br />
-							<Input
-								name="firstName"
-								type="text"
-								onChange={handleChange}
-								required="required"
-								maxLength="40"
-							/>
-						</div>
-						<div>
-							Last Name <br />
-							<Input
-								name="lastName"
-								type="text"
-								onChange={handleChange}
-								required="required"
-								maxLength="40"
-							/>
-						</div>
-						<div>
-							Street Address <br />
-							<Input
-								name="address"
-								type="text"
-								onChange={handleChange}
-								required="required"
-								maxLength="128"
-							/>
-						</div>
-						<div>
-							Unit / Apt <br />
-							<Input
-								name="unit"
-								type="text"
-								onChange={handleChange}
-								maxLength="128"
-							/>
-						</div>
-						<div>
-							State / Territory / Province <br />
-							<Dropdown
-								name="state"
-								onChange={handleChange}
-								defaultOption="Select a State"
-								options={states}
-								required="required"
-							/>
-						</div>
-						<div>
-							City <br />
-							<Dropdown
-								name="city"
-								onChange={handleChange}
-								defaultOption="Select a State Before"
-								options={cities && cities}
-								required="required"
-							/>
-						</div>
-						<div>
-							Email <br />
-							<Input
-								name="email"
-								type="email"
-								onChange={handleChange}
-								required="required"
-								maxLength="128"
-							/>
-						</div>
-						<div>
-							<Button type="submit" label="Entrar" />
-						</div>
-					</form>
+
+			{success && <div className="container">{message}</div>}
+
+			{!success && (
+				<div className="container">
+					<div>{message}</div>
+					<div className="formContact">
+						<form onSubmit={handleSubmit}>
+							<div>
+								First Name <br />
+								<Input
+									name="firstName"
+									type="text"
+									onChange={handleChange}
+									required="required"
+									maxLength="40"
+								/>
+							</div>
+							<div>
+								Last Name <br />
+								<Input
+									name="lastName"
+									type="text"
+									onChange={handleChange}
+									required="required"
+									maxLength="40"
+								/>
+							</div>
+							<div>
+								Street Address <br />
+								<Input
+									name="address"
+									type="text"
+									onChange={handleChange}
+									required="required"
+									maxLength="128"
+								/>
+							</div>
+							<div>
+								Unit / Apt <br />
+								<Input
+									name="unit"
+									type="text"
+									onChange={handleChange}
+									maxLength="128"
+								/>
+							</div>
+							<div>
+								State / Territory / Province <br />
+								<Dropdown
+									name="state"
+									onChange={handleChange}
+									defaultOption="Select a State"
+									options={states}
+									required="required"
+								/>
+							</div>
+							<div>
+								City <br />
+								<Dropdown
+									name="city"
+									onChange={handleChange}
+									defaultOption="Select a State Before"
+									options={cities && cities}
+									required="required"
+								/>
+							</div>
+							<div>
+								Email <br />
+								<Input
+									name="email"
+									type="email"
+									onChange={handleChange}
+									required="required"
+									maxLength="128"
+								/>
+							</div>
+							<div>
+								<Button type="submit" label="Entrar" />
+							</div>
+						</form>
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 }
